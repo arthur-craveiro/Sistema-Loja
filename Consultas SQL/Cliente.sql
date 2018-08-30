875535bd32937847bd85fc7a4b9758c03369df90
FelipeSantiagoIFRN <josefran.assis@hotmail.com>
2018-08-30 00:32:40 -0300

    Consulta sql

--- Consultas SQL/Cliente.sql
+++ Consultas SQL/Cliente.sql
@@ -1,5 +1,5 @@
-create table Fornecedor (
-	idFornecedores int identity not null,
+create table Cliente (
+	idCliente int identity not null,
 	nome varchar(50) not null,
 	telefones varchar(100) not null,
 	cidade varchar(50) not null,
@@ -7,9 +7,7 @@ create table Fornecedor (
 	CPF varchar(14),
 	CNPJ varchar(18),
 	idestado int not null,
-	primary key(idFornecedores),
+	primary key(idCliente),
 	foreign key(idestado) references UF(id)
 )
 
-insert into Fornecedor(nome, telefones, cidade, endereco, CPF, CNPJ, idestado) values ('Otávio Lima Ponce', '+055 (84) 92424-2424', 'Natal', 'Rua dos baitolas, 24, Bairro Sou gay e dou o cu', '881.482.180-13', '12.495.482/0001-87', 20);
-select * from Fornecedor;
